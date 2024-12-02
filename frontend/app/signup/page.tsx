556dc7e04a3d4bf8731f8ff42f05a45cd7f89698
damepsd <damepsd12@gmail.com>
2024-12-02 14:39:33 +0100

    j'ai mal a la tete

--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -2,7 +2,6 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import Fadjmalogo from '../public/fadjmalogo.png';
 import styled from 'styled-components';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
